57d8e87c285eb636161213eb1e7642f934ad5946
Grisha Angelov <grisha.angelov89@gmail.com>
2020-11-23 15:21:18 +0200

    Add app structure

---
+++ src/App.spec.js
@@ -0,0 +1,12 @@
+import { shallow } from 'enzyme';
+import App from './App'
+
+describe('(Component) App', () => {
+  it('should render component', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.equals(
+      <div>test</div>
+    )).to.equal(true)
+  });
+});
