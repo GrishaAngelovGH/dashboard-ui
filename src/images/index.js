3b9033d64d00d4c0dcc327d34dd44f08f4b4ec34
Grisha Angelov <grisha.angelov89@gmail.com>
2020-11-25 15:57:06 +0200

    Add data type images

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,35 @@
 import dashboardsOverview from './dashboards-overview.gif'
 
+import azure from './azure.svg'
+import csv from './csv.svg'
+import db2 from './db2.svg'
+import dbase from './dbase.svg'
+import excel from './excel.svg'
+import firebird from './firebird.svg'
+import google from './google.svg'
+import informix from './informix.svg'
+import json from './json.svg'
+import mongo from './mongo.svg'
+import msaccess from './msaccess.svg'
+import mssql from './mssql.svg'
+import mysql from './mysql.svg'
+import odata from './odata.svg'
+import odbc from './odbc.svg'
+import oracle from './oracle.svg'
+import postgre from './postgre.svg'
+import sqlite from './sqlite.svg'
+import sybase from './sybase.svg'
+import teradata from './teradata.svg'
+import vistadb from './vistadb.svg'
+import xml from './xml.svg'
+
+const dataTypes = {
+    azure, csv, db2, dbase, excel, firebird, google, informix,
+    json, mongo, msaccess, mssql, mysql, odata, odbc, oracle,
+    postgre, sqlite, sybase, teradata, vistadb, xml
+}
 
 export {
-    dashboardsOverview
+    dashboardsOverview,
+    dataTypes
 }
