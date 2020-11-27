b1dad47a54ba19d7dd3a965dc412e35c2dddc6e1
Grisha Angelov <grisha.angelov89@gmail.com>
2020-11-27 17:26:29 +0200

    Implement DashboardPage

--- src/components/Pages/DashboardPage/Dashboard.spec.js
+++ src/components/Pages/DashboardPage/Dashboard.spec.js
@@ -1,6 +1,5 @@
-import Layout from 'components/Layout'
-
 import Menu from './Menu'
+import ProgressWidget from './ProgressWidget'
 import DashboardPage from './DashboardPage'
 
 describe('(Component) DashboardPage', () => {
@@ -8,7 +7,44 @@ describe('(Component) DashboardPage', () => {
         const wrapper = shallow(<DashboardPage />)
 
         expect(wrapper.equals(
-            <Layout header={<Menu />} body={<span>body</span>} />
+            <div className='row'>
+                <div className='col-md-12'>
+                    <div className='row no-gutters'>
+                        <div className='col-md-12'>
+                            <Menu />
+                        </div>
+                    </div>
+                    <div className='row container mx-md-auto'>
+                        <div className='col-md-4 offset-md-3 offset-lg-0 mt-3'>
+                            <ProgressWidget
+                                title={'Total Orders'}
+                                subtitle={'Last year expenses'}
+                                value={'1896'}
+                                progress={65}
+                                label={'YoY Growth'}
+                            />
+                        </div>
+                        <div className='col-md-4 offset-md-3 offset-lg-0 mt-3'>
+                            <ProgressWidget
+                                title={'Products Sold'}
+                                subtitle={'Revenue streams'}
+                                value={'$3M'}
+                                progress={85}
+                                label={'Sales'}
+                            />
+                        </div>
+                        <div className='col-md-4 offset-md-3 offset-lg-0 mt-3'>
+                            <ProgressWidget
+                                title={'Followers'}
+                                subtitle={'People interested'}
+                                value={'45,9%'}
+                                progress={46}
+                                label={'Twitter Progress'}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         )).to.equal(true)
     })
 })
--- src/components/Pages/DashboardPage/DashboardPage.js
+++ src/components/Pages/DashboardPage/DashboardPage.js
@@ -1,8 +1,37 @@
-import Layout from 'components/Layout'
 import Menu from './Menu'
+import ProgressWidget from './ProgressWidget'
+
+const progressWidgets = [
+    { title: 'Total Orders', subtitle: 'Last year expenses', value: '1896', progress: 65, label: 'YoY Growth' },
+    { title: 'Products Sold', subtitle: 'Revenue streams', value: '$3M', progress: 85, label: 'Sales' },
+    { title: 'Followers', subtitle: 'People interested', value: '45,9%', progress: 46, label: 'Twitter Progress' }
+]
 
 const DashboardPage = () => (
-    <Layout header={<Menu />} body={<span>body</span>} />
+    <div className='row'>
+        <div className='col-md-12'>
+            <div className='row no-gutters'>
+                <div className='col-md-12'>
+                    <Menu />
+                </div>
+            </div>
+            <div className='row container mx-md-auto'>
+                {
+                    progressWidgets.map(v => (
+                        <div key={v.title} className='col-md-4 offset-md-3 offset-lg-0 mt-3'>
+                            <ProgressWidget
+                                title={v.title}
+                                subtitle={v.subtitle}
+                                value={v.value}
+                                progress={v.progress}
+                                label={v.label}
+                            />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
 )
 
 export default DashboardPage
