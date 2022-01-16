082acb30ce1c2890b4dda19b3ceb22510acfb08e
Grisha Angelov <grisha.angelov89@gmail.com>
2022-01-16 13:04:46 +0200

    Add margin in DashboardOverviewSection

--- src/components/Pages/HomePage/PageSections/DashboardOverviewSection/DashboardOverviewSection.js
+++ src/components/Pages/HomePage/PageSections/DashboardOverviewSection/DashboardOverviewSection.js
@@ -1,15 +1,13 @@
-import LinkButton from 'components/LinkButton'
 import PageSection from 'components/Pages/PageSection'
-
 import { dashboardsOverview } from 'images'
 
 const DashboardOverviewSection = () => (
     <PageSection color='#0b3d7f' height={520}>
         <div className='row no-gutters'>
             <div className='col-md-12 mt-2'>
-                <div className='row no-gutters justify-content-center mb-2 text-white'>
+                <div className='row no-gutters justify-content-center m-3'>
                     <div className='col-md-8 text-center'>
-                        <h2>Dashboards</h2>
+                        <h2 className='text-white'>Dashboards</h2>
                     </div>
                 </div>
 
@@ -19,17 +17,11 @@ const DashboardOverviewSection = () => (
                         intelligence tool for data analysis. Dashboards is the best
                         solution for displaying metrics in an understandable and readable form.
                         You may embed our dashboards into your application.
-                   </div>
-                </div>
-
-                <div className='row no-gutters justify-content-center'>
-                    <div className='col-md-5 col-8'>
-                        <LinkButton href='#/'>Install Dashboards</LinkButton>
                     </div>
                 </div>
 
-                <div className='row no-gutters justify-content-center mt-2'>
-                    <div className='col-md-4 col-lg-3'>
+                <div className='row no-gutters justify-content-center mt-3'>
+                    <div className='col-md-4 col-lg-5'>
                         <img src={dashboardsOverview} className='img-fluid' />
                     </div>
                 </div>
