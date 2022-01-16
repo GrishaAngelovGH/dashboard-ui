02da51f51405fe7f4ab1677b7b6366f55fc84545
Grisha Angelov <grisha.angelov89@gmail.com>
2022-01-16 13:00:37 +0200

    Refactor Menu in DashboardPage

--- src/components/Pages/DashboardPage/Menu/Menu.spec.js
+++ src/components/Pages/DashboardPage/Menu/Menu.spec.js
@@ -1,52 +1,24 @@
-import Badge from 'react-bootstrap/Badge'
-import Button from 'react-bootstrap/Button'
-import { Navbar, Nav } from 'react-bootstrap'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-
-import Menu, {
-    SearchIcon,
-    FolderIcon,
-    CalendarDateIcon,
-    BellIcon,
-    GraphUpIcon,
-    DisplayIcon
-} from './Menu'
+import Menu from './Menu'
+import { Nav, Navbar } from 'react-bootstrap'
 
 describe('(Component) Menu', () => {
     it('should render component', () => {
         const wrapper = shallow(<Menu />)
 
         expect(wrapper.equals(
-            <Navbar bg='nav-color' expand='lg' className='shadow p-3 mb-5 bg-white rounded'>
-                <Navbar.Brand>Dashboard UI</Navbar.Brand>
-
+            <Navbar bg='nav-color' expand='lg'>
+                <Navbar.Brand href='#'>Dashboard UI</Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
-
                 <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='mr-auto w-100 d-flex justify-content-around'>
-                        <Button variant='primary'>
-                            <SearchIcon />  Search
-                </Button>
 
-                        <DropdownButton id='id1' variant='outline-primary' title='Categories' />
-                        <DropdownButton id='id2' variant='outline-primary' title={<span><FolderIcon /><span className='ml-1'>Files</span></span>} />
-                        <DropdownButton id='id3' variant='outline-primary' title={<span><Badge pill variant='primary'>3</Badge> Projects</span>} />
-
-                        <Button variant='outline-primary'>
-                            <CalendarDateIcon />
-                        </Button>
-
-                        <Button variant='outline-primary'>
-                            <BellIcon />
-                        </Button>
-
-                        <Button variant='outline-primary'>
-                            <GraphUpIcon />
-                        </Button>
-
-                        <Button variant='outline-primary'>
-                            <DisplayIcon />
-                        </Button>
+                    <Nav className='navbar-nav ml-auto'>
+                        <Nav.Link href='#'>Dashboard</Nav.Link>
+                        <Nav.Link href='#'>Overview</Nav.Link>
+                        <Nav.Link href='#'>Chat</Nav.Link>
+                        <Nav.Link href='#'>Team</Nav.Link>
+                        <Nav.Link href='#'>Tasks</Nav.Link>
+                        <Nav.Link href='#'>Reports</Nav.Link>
+                        <Nav.Link href='#'>Settings</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
