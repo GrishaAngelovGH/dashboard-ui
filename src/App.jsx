af59679b661877fe68eb066bc7bcb3b6f471bb2d
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-18 09:01:07 +0200

    Change routing configuration to use BrowserRouter by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { HomePage, DashboardPage } from 'components/Pages'
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} />
         <Route path='/dashboard' element={<DashboardPage />} />
       </Routes>
     </Router>
