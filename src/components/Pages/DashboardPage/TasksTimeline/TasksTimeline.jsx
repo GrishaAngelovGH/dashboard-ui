1a9f872cd7982e848db7e44d5140c815f019dc03
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-18 09:25:49 +0200

    Pass the correct structure of items in TasksTimeline according the latest changes in antd's Timeline

--- src/components/Pages/DashboardPage/TasksTimeline/TasksTimeline.jsx
+++ src/components/Pages/DashboardPage/TasksTimeline/TasksTimeline.jsx
@@ -1,18 +1,18 @@
-import Timeline from 'antd/lib/timeline'
+import { Timeline } from 'antd'
 import { ClockCircleOutlined } from '@ant-design/icons'
 
 const TasksTimeline = () => (
 	<Timeline mode='alternate' items={[
-		{ label: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nisi?' },
-		{ label: 'Lorem ipsum dolor sit amet.', color: 'green' },
+		{ children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nisi?' },
+		{ children: 'Lorem ipsum dolor sit amet.', color: 'green' },
 		{
-			label: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eaque quam, aperiam iste vel ipsum laudantium debitis ratione maxime voluptates sequi reprehenderit repellendus rerum nihil distinctio exercitationem vero inventore magni non. Ipsam ipsum maiores consequatur cupiditate excepturi, neque quam dolor facere quos magnam ea sed maxime expedita vitae nobis aliquam?',
+			children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eaque quam, aperiam iste vel ipsum laudantium debitis ratione maxime voluptates sequi reprehenderit repellendus rerum nihil distinctio exercitationem vero inventore magni non. Ipsam ipsum maiores consequatur cupiditate excepturi, neque quam dolor facere quos magnam ea sed maxime expedita vitae nobis aliquam?',
 			dot: <ClockCircleOutlined style={{ fontSize: '16px' }} />
 		},
-		{ label: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, vel!', color: 'red' },
-		{ label: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At assumenda id eius. Cupiditate natus reprehenderit sint corrupti dolore provident molestias.' },
+		{ children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, vel!', color: 'red' },
+		{ children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At assumenda id eius. Cupiditate natus reprehenderit sint corrupti dolore provident molestias.' },
 		{
-			label: 'Lorem ipsum dolor sit amet.',
+			children: 'Lorem ipsum dolor sit amet.',
 			dot: <ClockCircleOutlined style={{ fontSize: '16px' }} />
 		}
 	]} />
